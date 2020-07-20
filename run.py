e32a51e255f95797bb4a3c1b1c0b3ee3f3937a84
tracyjohnson213 <tracyjohnson213@gmail.com>
2020-07-20 02:49:12 +0000

    render homepage with connection to database

--- run.py
+++ run.py
@@ -1,12 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'myCookeys'
+# TO-DO remove uri from code
+# app.config["MONGO_URI"] = os.getenv('MONGO_URI',
+#                                     'mongodb://localhost:27017/myCookeys')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI',
+                                    "mongodb+srv://tracyj:1studentDeveloper@firstcluster.b5ihz.mongodb.net/myCookeys?retryWrites=true&w=majority")
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return "Hello, World"
+@app.route('/get_recipes')
+def get_recipes():
+    """ render all recipes """
+    return render_template('recipes.html',
+                           recipes=mongo.db.recipes.find(),
+                           title='Recipes')
 
 
 if __name__ == '__main__':
